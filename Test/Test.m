0c20dab922ea0fb10f5841a2ebd8d88df4ea12c7
Sven A. Schmidt <sas@abstracture.de>
2011-07-11 17:40:09 +0200

    Added test files with different line endings

--- Test/Test.m
+++ Test/Test.m
@@ -12,19 +12,6 @@
 
 @implementation Test
 
-- (void)setUp
-{
-    [super setUp];
-    
-    // Set-up code here.
-}
-
-- (void)tearDown
-{
-    // Tear-down code here.
-    
-    [super tearDown];
-}
 
 - (void)testParsing {
   NSBundle *thisBundle = [NSBundle bundleForClass:[self class]];
@@ -38,8 +25,40 @@
   //NSArray *objs = [data csvRowsWithSeparator:@";"];
   NSArray *objs = [data arrayWithSeparator:@";"];
   STAssertEquals(36u, [objs count], @"objs count");
-  
-
+  STAssertEqualObjects(@"Wadgassen", [[objs objectAtIndex:0] objectForKey:@"Ort"], @"value for 'Ort'");
 }
+
+
+- (void)testParsingWindowsLineEndings {
+  NSBundle *thisBundle = [NSBundle bundleForClass:[self class]];
+  NSURL *url = [thisBundle URLForResource:@"kunden_win" withExtension:@"csv"];
+  STAssertNotNil(url, @"url must not be nil");
+  
+  NSError *error = nil;
+  NSString *data = [NSString stringWithContentsOfURL:url encoding:NSUTF8StringEncoding error:&error];
+  STAssertNotNil(data, @"data must not be nil");
+  
+  //NSArray *objs = [data csvRowsWithSeparator:@";"];
+  NSArray *objs = [data arrayWithSeparator:@";"];
+  STAssertEquals(36u, [objs count], @"objs count");
+  STAssertEqualObjects(@"Wadgassen", [[objs objectAtIndex:0] objectForKey:@"Ort"], @"value for 'Ort'");
+}
+
+
+- (void)testParsingMacLineEndings {
+  NSBundle *thisBundle = [NSBundle bundleForClass:[self class]];
+  NSURL *url = [thisBundle URLForResource:@"kunden_mac" withExtension:@"csv"];
+  STAssertNotNil(url, @"url must not be nil");
+  
+  NSError *error = nil;
+  NSString *data = [NSString stringWithContentsOfURL:url encoding:NSUTF8StringEncoding error:&error];
+  STAssertNotNil(data, @"data must not be nil");
+  
+  //NSArray *objs = [data csvRowsWithSeparator:@";"];
+  NSArray *objs = [data arrayWithSeparator:@";"];
+  STAssertEquals(36u, [objs count], @"objs count");
+  STAssertEqualObjects(@"Wadgassen", [[objs objectAtIndex:0] objectForKey:@"Ort"], @"value for 'Ort'");
+}
+
 
 @end
